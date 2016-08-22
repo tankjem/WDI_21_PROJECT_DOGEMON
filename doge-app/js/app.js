327b6e3a986ad33202fe76a4ed2f7c8aa0240344
Jeremy Smith <jmsmith57@hotmail.com>
2016-08-22 10:42:11 +0100

    working on auth views and routes

--- doge-app/js/app.js
+++ doge-app/js/app.js
@@ -1,3 +1,102 @@
+
+var DogeApp = DogeApp || {};
+
+DogeApp.API_URL = "http://localhost:3000/api";
+
+DogeApp.setRequestHeader = function(jqXHR) {
+  var token = window.localStorage.getItem("token");
+  if(!!token) return jqXHR.setRequestHeader('Authorization', 'Bearer ' + token);
+}
+
+DogeApp.getTemplate = function(template, data) {
+  return $.get('/templates/' + template + '.html').done(function(templateHtml) {
+    var html = _.template(templateHtml)(data);
+    DogeApp.$main.html(html);
+    DogeApp.updateUI();
+  });
+}
+DogeApp.getUser = function() {
+  event.preventDefault();
+
+  var id = $(this).data('id');
+
+  return $.ajax({
+    method: "GET",
+    url: DogeApp.API_URL + "/users/" + id
+  }).done(function(data) {
+    DogeApp.getTemplate("show", { user: data });
+  });
+}
+DogeApp.handleForm = function() {
+  event.preventDefault();
+
+  $(this).find('button').prop('disabled', true);
+
+  var data = $(this).serialize();
+  var method = $(this).attr("method");
+  var url = DogeApp.API_URL + $(this).attr("action");
+
+  return $.ajax({
+    url: url,
+    method: method,
+    data: data,
+    beforeSend: DogeApp.setRequestHeader
+  })
+  .done(function(data){
+    if(!!data.token) {
+      window.localStorage.setItem("token", data.token);
+    }
+
+    DogeApp.getUser();
+  })
+  .fail(DogeApp.handleFormErrors);
+}
+
+DogeApp.handleFormErrors = function(jqXHR) {
+  var $form = $("form");
+  for(field in jqXHR.responseJSON.errors) {
+    $form.find("input[name=" + field + "]").parents('.form-group').addClass('has-error');
+  }
+  $form.find('button').removeAttr('disabled');
+}
+
+DogeApp.getEditForm = function() {
+  event.preventDefault();
+
+  var id = $(this).data('id');
+
+  return $.get(DogeApp.API_URL + "/users/" + id).done(function(data) {
+    DogeApp.getTemplate("edit", { user: data });
+  });
+}
+
+DogeApp.deleteUser = function() {
+  event.preventDefault();
+
+  var id = $(this).data('id');
+
+  return $.ajax({
+    method: "DELETE",
+    url: DogeApp.API_URL + "/users/" + id,
+    beforeSend: DogeApp.setRequestHeader
+  }).done(DogeApp.getRegister);
+}
+
+DogeApp.loadPage = function() {
+  event.preventDefault();
+  DogeApp.getTemplate($(this).data('template'));
+}
+
+DogeApp.logout = function(){
+  event.preventDefault();
+  window.localStorage.clear();
+  DogeApp.updateUI();
+}
+
+
+
+
+
 var map = new google.maps.Map(document.getElementById('map'), 
 {
   center: { lat: 51.5080072, lng: -0.1019284 },
@@ -29,3 +128,4 @@ navigator.geolocation.getCurrentPosition(function(position) {
  map.panTo(marker.getPosition());
  map.setZoom(17);
 });
+
