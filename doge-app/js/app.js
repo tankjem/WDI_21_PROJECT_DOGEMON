2050bc8aa1eb371e2c27fa5977046b3c4d321e5d
Bex B <bexb1@hotmail.com>
2016-08-23 18:07:24 +0100

    Minor fixes.

--- doge-app/js/app.js
+++ doge-app/js/app.js
@@ -475,3 +475,32 @@ navigator.geolocation.getCurrentPosition(function(position) {
 //     inventory.setAttribute("class","items");
 //   }
 // }
+
+var marker = null;
+
+function autoUpdate() {
+  navigator.geolocation.getCurrentPosition(function(position) {  
+    var newPoint = new google.maps.LatLng(position.coords.latitude, 
+                                          position.coords.longitude);
+
+    if (marker) {
+      // Marker already created - Move it
+      marker.setPosition(newPoint);
+    }
+    else {
+      // Marker does not exist - Create it
+      marker = new google.maps.Marker({
+        position: newPoint,
+        map: map
+      });
+    }
+
+    // Center the map on the new position
+    map.setCenter(newPoint);
+  }); 
+
+  // Call the autoUpdate() function every 5 seconds
+  setTimeout(autoUpdate, 5000);
+}
+
+autoUpdate();
