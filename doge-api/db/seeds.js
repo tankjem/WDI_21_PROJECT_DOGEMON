ed775cb5641c96c16cb8f335d5e36372e632c049
Bex B <bexb1@hotmail.com>
2016-08-24 19:38:21 +0100

    Menu styling.

--- doge-api/db/seeds.js
+++ doge-api/db/seeds.js
@@ -311,8 +311,8 @@ Item.create([{
   image_url: "https://i.ytimg.com/vi/hEJnMQG9ev8/maxresdefault.jpg",
   health: 69,
   inventory: [items[6], items[7], items[8], items[9], items[10], items[5]],
-  hunger: 37,
-  thirst: 45,
+  food: 37,
+  water: 45,
   armour: 10
   })
 }).then(function(pc1) {
