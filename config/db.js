d8c25fc9fa968b97b6b1c920a80eeaecded709af
Bex B <bexb1@hotmail.com>
2016-08-25 16:15:57 +0100

    Updated mongolab environment var

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/doge-api--test",
   development: "mongodb://localhost/doge-api",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/doge-api"
+  production: process.env.MONGODB_URI || "mongodb://localhost/doge-api"
 }
 
 module.exports = function(env) {
